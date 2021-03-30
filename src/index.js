0332aa94314d83fe6fdca3ffa2b94b0a4fd8c326
Elizaveta Panasiuk <lizaveta.panasiuk@gmail.com>
2021-03-30 13:53:52 +0300

    fix: thunk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './redux/reducer';
+import thunkMiddleware from "redux-thunk";
+import 'redux-thunk';
 
-const store = createStore(reducer);
-
+const store = createStore(reducer,  applyMiddleware(thunkMiddleware));
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- src/redux/action.js
+++ src/redux/action.js
@@ -1,34 +1,23 @@
 export const writeText = () => {
-/*   return (dispatch) => {
+  return async(dispatch) => {
     dispatch({type: 'WRITE_TEXT'});
-  }; */
-  return{
-    type: 'WRITE_TEXT'
-  }
+  };
 };
 
 export const clearOutput = () => {
-/*   return (dispatch) => {
+  return async (dispatch) => {
     dispatch({type: 'CLEAR_OUTPUT'});
-  } */
-  return{
-    type: 'CLEAR_OUTPUT'
   }
 }
 export const clearInput = () => {
-/*   return (dispatch) => {
+  return async (dispatch) => {
     dispatch({type: 'CLEAR_INPUT'});
-  } */
-  return{
-    type: 'CLEAR_INPUT'
   }
+
 }
 
 export const inputText = (text) => {
-/*   return (dispatch) => {
+  return async (dispatch) => {
     dispatch({type: 'INPUT_TEXT', payload: text});
-  } */
-  return{
-    type: 'INPUT_TEXT', payload: text
   }
 }
